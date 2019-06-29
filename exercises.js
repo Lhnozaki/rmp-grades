1d22c230db91888fa590549ecf1b4f0e1d729208
Lewis Nozaki <lhnozaki@gmail.com>
2019-06-28 20:34:07 -1000

    completed

--- exercises.js
+++ exercises.js
@@ -15,4 +15,99 @@
 // 9. generates a new score list with a 5% increase for each score 
 // 10.generates a list of scores that are odd numbered
 
+let scoreModule = (function() {
 
+let scores = [
+  65, 76, 100, 88, 17, 55
+];
+
+/// Add
+let add = function() {
+  let sum = 0;
+  for(i in scores) {
+    sum += scores[i];
+  }
+  return sum;
+}
+
+/// Average
+let avg = function() {
+  let mean = add() / scores.length;
+  return mean;
+}
+
+/// Highest
+let highest = function() {
+  return Math.max(...scores);
+}
+
+/// Lowest
+let lowest = function() {
+  return Math.min(...scores)
+}
+
+/// Random
+let randomNum = function() {
+  return Math.floor(Math.random() * 30);
+}
+
+/// Score of 70 or above
+let pass = function() {
+  let count = 0;
+  for(i in scores) {
+    if(scores[i] >= 70){
+      count++;
+    }
+  }
+  return count;
+}
+
+/// First Score
+let first = function() {
+  return scores.shift();
+}
+
+/// Last Score
+let last = function() {
+  return scores.pop();
+}
+
+/// Increase by 5%
+let increase = function() {
+  let arr = [];
+  for(i in scores) {
+    arr.push(scores[i] * .05 + scores[i])
+  }
+  return arr;
+}
+
+/// Odd scores
+
+let odd = function() {
+  let arrOdd = [];
+  for(i in scores) {
+    if(scores[i] % 2 === 1) {
+      arrOdd.push(scores[i])
+    }
+  }
+  return arrOdd;
+}
+
+return {
+  add: add,
+  avg: avg,
+  highest: highest,
+  lowest: lowest,
+  randomNum: randomNum,
+  pass: pass,
+  first: first,
+  last: last,
+  increase: increase,
+  odd: odd
+}
+
+
+
+})();
+
+console.log(scoreModule.odd());
